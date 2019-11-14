e391c1a3fa9c38b92f28e30e9b027d8fb0c8dcdf
Tyson <02tyson.watson@gulllakecs.org>
2019-11-14 09:54:59 -0500

    Fixed the calling of Roll a Six

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will indent my print statement
             }
         }
-        rollASix()
+        rollASix();
         countBlocks(7);
     }
 }
