820a1be41f971df056db1cba115684d775e7efb8
Tyson <02tyson.watson@gulllakecs.org>
2019-11-14 09:38:57 -0500

    Added Count Blocks Function
    many blocks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,14 @@
 package com.company;
 
 public class Main {
+    public static void countBlocks(int levels) {
+        int total = 0;
+        for (int i = 1; i <= levels; i++) {
+            total = total + (i * i);
+        }
+        System.out.println(total);
+    }
+
 
     public static void main(String[] args) {
         for(int i=0; i<3; i++){
@@ -9,5 +17,6 @@ public class Main {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will indent my print statement
             }
         }
+        countBlocks(7);
     }
 }
