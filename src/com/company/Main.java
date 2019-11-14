ad117cffd40db4de6712d72f20938dfbe7c1f313
Tyson <02tyson.watson@gulllakecs.org>
2019-11-14 09:43:19 -0500

    Added Roll a Six Function
    I won!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class Main {
+    public static void rollASix() {
+        int rollDice = 0;
+
+        while (rollDice != 6) {
+            rollDice = (int) (Math.random() * 6 + 1);
+            System.out.println("You rolled: " + rollDice);
+
+            if (rollDice == 3) {
+                break;
+            }
+        }
+        if (rollDice == 6) {
+            System.out.println("You Win!!!");
+        } else {
+            System.out.println("You Lose");
+        }
+    }
 
     public static void main(String[] args) {
         for(int i=0; i<3; i++){
@@ -9,5 +26,6 @@ public class Main {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will indent my print statement
             }
         }
+        rollASix();
     }
 }
